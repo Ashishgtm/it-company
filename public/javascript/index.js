fb116805a68c3676005dd5ba4180a0fede534ff8
Ashish Gtm <62847647+Ashishgtm@users.noreply.github.com>
2021-08-23 20:58:36 +0545

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -14,3 +14,14 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+$(document).ready(function() {
+  $('.swiper-slide img').height($(window).width()/1.77*0.7)
+  $('.swiper-slide .overlay .text').css({'font-size':30*($(window).width()/1820)})
+})
+
+$(window).resize(function() {
+  $('.swiper-slide img').height($(window).width()/1.77*0.7)
+  $('.swiper-slide .overlay .text').css({'font-size':30*($(window).width()/1820)})
+})
