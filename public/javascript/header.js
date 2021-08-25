b46c74ddb45a1d0afd31de52a1fd6e77757d6031
Ashish Gtm <62847647+Ashishgtm@users.noreply.github.com>
2021-08-25 14:05:24 +0545

    Update header.js

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $('.dropdown-menu').width($(window).width()*0.5);
+    dropdownMenuWidth();
     $('body').css({'margin-top':$('.navbar-fixed-top').height()});
 });
 $(window).resize(function() {
-    $('.dropdown-menu').width($(window).width()*0.5);
+    dropdownMenuWidth();
 })
 function dropdownMenuWidth() {
     if($(window).width()>768) $('.dropdown-menu').width($(window).width()*0.5);
-    else $('.dropdown-menu').width($(window).width()*0.8);
+    else $('.dropdown-menu').width($(window).width()*0.9);
 }
