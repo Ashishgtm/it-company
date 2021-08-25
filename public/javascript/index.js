99e5bc99d800ddf3f813aba5d5a91f76a932c755
Ashish Gtm <62847647+Ashishgtm@users.noreply.github.com>
2021-08-25 21:19:31 +0545

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,19 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  
 
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-    
-  },
-  
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
 
 
 $(document).ready(function() {
